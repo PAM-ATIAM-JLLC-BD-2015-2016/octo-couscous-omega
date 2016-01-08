d53c34ac626da005efb2cb2d2a852a99545509c7
tbazin <theis.bazin@ens-cachan.fr>
2016-01-08 18:04:00 +0100

    Added dissipation matrix computation function

---
+++ matlab/modal_coefficients/F_compute_dissipation_m.m
@@ -0,0 +1,23 @@
+function [ dissipation_m ] = F_compute_dissipation_m( ...
+    string_modes_n, body_modes_n, string_tension, string_length, ...
+    string_bending_stiffness, string_linear_mass, ...
+    effective_stiffnesses_v, effective_masses_v, ...
+    string_q_factors_v, body_q_factors_v )
+%% Dissipation matrix, C, computation
+% 
+
+%% String block
+dissipation_1_1_v = pi*sqrt(string_tension*string_linear_mass)/2 * ...
+    (1:string_modes_n) ./ string_q_factors_v.';
+dissipation_1_1_m = diag(dissipation_1_1_v);
+
+%% Body block
+dissipation_2_2_v = ...
+    sqrt(effective_stiffnesses_v.*effective_masses_v) ./ body_q_factors_v;
+dissipation_2_2_m = diag(dissipation_2_2_v);
+
+%% Block-wise matrix definition
+dissipation_m = blkdiag(dissipation_1_1_m, dissipation_2_2_m);
+
+end
+
