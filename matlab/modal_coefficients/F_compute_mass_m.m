52c1fef0fba047049353738122a0ff103299d16e
tbazin <theis.bazin@ens-cachan.fr>
2016-01-08 18:04:25 +0100

    Minor cosmetic update

--- matlab/modal_coefficients/F_compute_mass_m.m
+++ matlab/modal_coefficients/F_compute_mass_m.m
@@ -1,26 +1,25 @@
 function [ mass_m ] = F_compute_mass_m( string_modes_n, body_modes_n, ...
     effective_masses_v, string_linear_mass, string_length )
-%% Mass matrix computation
+%% Mass matrix, M, computation
 % 
 
 string_mass = string_linear_mass*string_length; 
 
-%% String part of the mass matrix
+%% String block
 mass_1_1_v = string_mass/2 * ones(string_modes_n,1);
 mass_1_1_m = diag(mass_1_1_v);
 
-%% Coupled contributions
+%% Coupled contributions block
 mass_1_2_v = (-1).^(0:string_modes_n-1) .* ...
     ((string_mass/pi) ./ (1:string_modes_n));
 mass_1_2_m = repmat(mass_1_2_v.', 1, body_modes_n);
 
-%% Body part of the mass matrix
+%% Body block
 mass_2_2_m = diag(effective_masses_v) + ...
     ones(body_modes_n) * string_mass/3;
 
-%% Complete matrix definition
+%% Block-wise matrix definition
 mass_m = [ mass_1_1_m, mass_1_2_m;
-           mass_1_2_m.', mass_2_2_m];
+           mass_1_2_m.', mass_2_2_m ];
 
 end
-
