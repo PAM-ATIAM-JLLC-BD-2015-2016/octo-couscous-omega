9d542f2e4dfea53dfe8353e03b58ac55c1407382
VictorLR <rosivictor2@gmail.com>
2016-01-11 16:36:28 +0100

    re

--- matlab/modele_corde.m
+++ matlab/modele_corde.m
@@ -26,11 +26,11 @@ parameters=[string_length string_linear_mass];
 
 %% Détermination des modes
 string_wave_number=zeros(1,string_modes_n); % Pré-allocation
+
 for n=1:string_modes_n
-    %k_n(n)=n*pi/L;
     string_frequency(n) = (n*pi*celerity/string_length)*(1+(string_bending_stiffness/(2*string_tension))*(n*pi/string_length)^2);   
 end
-%w_n=k_n*c;
+
 string_wave_number = string_frequency/celerity;
 
 %% Damping coefficient as described in Woodhouse (b)
@@ -49,7 +49,7 @@ end
 yn = zeros(string_modes_n,length(t));
 y = zeros(1,length(t));
 for n = 1:string_modes_n
-yn(n,:)=sin(k_n(n)*excitation_point)*sin(k_n(n)*xe)*((initial_height/(string_length-excitation_point))+(initial_height/excitation_point))/(k_n(n))*cos(w_n(n)*t).*exp(-string_damping_coeff(n)*w_n(n)*t);
+yn(n,:)=sin(k_n(n)*excitation_point)*sin(k_n(n)*xe)*((initial_height/(string_length-excitation_point))+(initial_height/excitation_point))/(string_wave_number(n))*cos(string_frequency(n)*t).*exp(-string_damping_coeff(n)*string_frequency(n)*t);
 y = y+yn(n,:);
 end
 
@@ -61,7 +61,7 @@ for n =1:string_modes_n
   TMP(n,:) = (2*w - 1i*string_frequency(n)*string_damping_coeff(n))./...
       (w.^2 - 1i*w*string_frequency(n)*string_damping_coeff(n) - (string_frequency(n)^2)*(1-(string_damping_coeff(n)^2)/4));
 end
-Z_string = -(1i*string_tension/string_length)*((1./w) + sum(TMP));  % String impédance equation (29)
+Z_string = -(1i*string_tension/string_length)*((1./w) + sum(TMP));  % String impedance equation (29)
 
 %% Transfer function displacement/displacement
 
