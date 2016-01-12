bc124f9f86a6d0b22f2895ec9f6799f78da7be01
tbazin <theis.bazin@ens-cachan.fr>
2016-01-12 17:32:31 +0100

    Cosmetic update

--- matlab/modal_coefficients/F_compute_mass_m.m
+++ matlab/modal_coefficients/F_compute_mass_m.m
@@ -1,22 +1,24 @@
-function [ mass_m ] = F_compute_mass_m( string_modes_n, body_modes_n, ...
-    string_linear_mass, string_length, effective_masses_v )
+function [ mass_m ] = F_compute_mass_m( ...
+    string_modes_number, body_modes_number, ...
+    string_length, string_linear_mass, ...
+    effective_masses_v )
 %% Mass matrix, M, computation
 % 
 
 string_mass = string_linear_mass*string_length; 
 
 %% String block
-mass_1_1_v = string_mass/2 * ones(string_modes_n,1);
+mass_1_1_v = string_mass/2 * ones(string_modes_number,1);
 mass_1_1_m = diag(mass_1_1_v);
 
 %% Coupled contributions block
-mass_1_2_v = (-1).^(0:string_modes_n-1) .* ...
-    ((string_mass/pi) ./ (1:string_modes_n));
-mass_1_2_m = repmat(mass_1_2_v.', 1, body_modes_n);
+mass_1_2_v = (-1).^(0:string_modes_number-1) .* ...
+    ((string_mass/pi) ./ (1:string_modes_number));
+mass_1_2_m = repmat(mass_1_2_v.', 1, body_modes_number);
 
 %% Body block
 mass_2_2_m = diag(effective_masses_v) + ...
-    ones(body_modes_n) * string_mass/3;
+    ones(body_modes_number) * string_mass/3;
 
 %% Block-wise matrix definition
 mass_m = [ mass_1_1_m, mass_1_2_m;
