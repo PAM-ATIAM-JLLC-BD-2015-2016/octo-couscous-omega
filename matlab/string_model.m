4df9923b3618ff4abeebc5bb5a35827f1b9e6a6a
tbazin <theis.bazin@ens-cachan.fr>
2016-01-13 09:33:54 +0100

    Updated string_model to add parameters container output

--- matlab/string_model.m
+++ matlab/string_model.m
@@ -1,6 +1,9 @@
-function [H_string,Z_string] = modele_corde(string_modes_n)
+function [ H_string, Z_string, string_params ] = ...
+    string_model( string_modes_n )
 
-%% Discretisation fréquentielle et temporelle
+string_params = {};
+
+%% Discretisation frï¿½quentielle et temporelle
 
 Fs = 44100;    % Sampling frequency
 dt = 1/Fs;     % time step
@@ -8,24 +11,37 @@ Tmax = 6;      % waving time
 t=0:dt:Tmax;   % time vector
 f = Fs*linspace(0,1,length(t));
 w = 2*pi*f;
-%% Caractéristiques de la corde
 
-string_length = 0.65;              
-string_linear_mass = 6.24*10^(-3);    
-%xi = 10^(-3);                                % damping 2*xi = 1/Q = eta  Q = facteur de qualité 
-string_bending_stiffness = 57e-6;            
-initial_height = 5*10^(-3);                        
-xe = string_length/8;                         % listening point
-string_tension = 71.6;                         
+%% String physical characteristics
+
+string_length = 0.65;
+string_linear_mass = 6.24*10^(-3);
+% xi = 10^(-3);  % Damping 2*xi = 1/Q = eta  Q = quality factor
+string_bending_stiffness = 57e-6;
+string_tension = 71.6;
 celerity = sqrt(string_tension/string_linear_mass);     
-excitation_point = string_length/4;                        
-%string_modes_n = 10;                                 % NUmber of modes
 
+%% Observation parameters
+initial_height = 5*10^(-3);
+x_listening = string_length/8;  % Listening point
+x_excitation = string_length/4;                        
+% string_modes_n = 10;  % Number of modes
+
+%% Parameters container
 parameters=[string_length string_linear_mass];
 
+string_params.string_length = string_length;
+string_params.string_linear_mass = string_linear_mass;
+string_params.string_bending_stiffness = string_bending_stiffness;
+string_params.string_tension = string_tension;
+string_params.celerity = celerity;
 
-%% Détermination des modes
-string_wave_number=zeros(1,string_modes_n); % Pré-allocation
+string_params.initial_height = initial_height;
+string_params.x_listening = x_listening;
+string_params.x_excitation = x_excitation;
+
+%% Dï¿½termination des modes
+string_wave_number=zeros(1,string_modes_n); % Pre-allocation
 
 for n=1:string_modes_n
     string_frequency(n) = (n*pi*celerity/string_length)*(1+(string_bending_stiffness/(2*string_tension))*(n*pi/string_length)^2);   
@@ -49,7 +65,7 @@ end
 yn = zeros(string_modes_n,length(t));
 y = zeros(1,length(t));
 for n = 1:string_modes_n
-yn(n,:)=sin(string_wave_number(n)*excitation_point)*sin(string_wave_number(n)*xe)*((initial_height/(string_length-excitation_point))+(initial_height/excitation_point))/(string_wave_number(n))*cos(string_frequency(n)*t).*exp(-string_damping_coeff(n)*string_frequency(n)*t);
+yn(n,:)=sin(string_wave_number(n)*x_excitation)*sin(string_wave_number(n)*x_listening)*((initial_height/(string_length-x_excitation))+(initial_height/x_excitation))/(string_wave_number(n))*cos(string_frequency(n)*t).*exp(-string_damping_coeff(n)*string_frequency(n)*t);
 y = y+yn(n,:);
 end
 
@@ -68,11 +84,11 @@ clear TMP
 %% Transfer function displacement/displacement
 
 for n =1:string_modes_n
-  TMP(n,:) = (-1)^n*(2*w*sin(n*pi*excitation_point/string_length))./...
+  TMP(n,:) = (-1)^n*(2*w*sin(n*pi*x_excitation/string_length))./...
       (w.^2 - 1i*w*string_frequency(n)*string_damping_coeff(n) - (string_frequency(n)^2)*(1-(string_damping_coeff(n)^2)/4));
 end
 
-H_string = excitation_point/string_length + (celerity/string_length)*sum(TMP);
+H_string = x_excitation/string_length + (celerity/string_length)*sum(TMP);
 
 %figure;
 %plot(f,abs(fft(y)));
