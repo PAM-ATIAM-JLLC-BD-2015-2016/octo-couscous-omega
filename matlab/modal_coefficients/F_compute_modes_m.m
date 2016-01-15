78cd21086eb31864f2a5c2ba3bd9fc4de18a4bd3
tbazin <theis.bazin@ens-cachan.fr>
2016-01-15 15:12:52 +0100

    Cosmetic update

--- matlab/modal_coefficients/F_compute_modes_m.m
+++ matlab/modal_coefficients/F_compute_modes_m.m
@@ -1,6 +1,6 @@
 function [ modes_m, complex_natural_frequencies_v ] = ...
-    F_compute_modes_m( dissipation_m, mass_m, stiffness_m ) %, ...
-%     varargin )
+    F_compute_modes_m( dissipation_m, mass_m, stiffness_m, ...
+    varargin )
 %% Coupled modes computation, possible using two different methods
 % Combine the physical matrices describing the system to compute the
 % vibration modes and frequencies.
@@ -12,26 +12,26 @@ function [ modes_m, complex_natural_frequencies_v ] = ...
 
 %% Arguments parsing
 p = inputParser;
-% 
-% defaultMode = 'ODE';
-% validModes = {'ODE', 'first-order'};
-% checkMode = @(type_s) any(validatestring(computation_mode_s, validModes));
+
+defaultMode = 'first-order';
+validModes = {'ODE', 'first-order'};
+checkMode = @(string) any(validatestring(string, validModes));
 
 addRequired(p, 'dissipation_m', @isnumeric);
 addRequired(p, 'mass_m', @isnumeric);
 addRequired(p, 'stiffness_m', @isnumeric);
-% addParameter(p, 'computation_mode_s', defaultType, checkMode);
+addParameter(p, 'computation_mode_s', defaultMode, checkMode);
 
-parse(p, dissipation_m, mass_m, stiffness_m); %, varargin{:});
+parse(p, dissipation_m, mass_m, stiffness_m, varargin{:});
 
-% if strcmp(computations_mode_s, 'ODE')
+% if strcmp(p.Results.computation_mode_s, 'ODE')
 % %% Undamped modes computation via physical differential equation
 % syms q(t);
 % Dq = diff(q);
 % D2q = diff(q, 2);
 % system_eqn = ( mass_m*D2q + dissipation_m*Dq + stiffness_m*q == 0);
 % dsolve(system_eqn);
-
+% 
 % else
 %% Damped modes computation via first-order reformulation of the system
 modes_number = length(mass_m);
