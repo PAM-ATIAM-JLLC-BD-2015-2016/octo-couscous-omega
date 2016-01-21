660153e289a409a9c42c6df2dd5dee13fc0fe661
tbazin <theis.bazin@ens-cachan.fr>
2016-01-21 14:28:31 +0100

    Fixed bug (due to an error in Woodhouse's paper) in combined_system_m

--- matlab/modal_coefficients/F_compute_modes_m.m
+++ matlab/modal_coefficients/F_compute_modes_m.m
@@ -1,7 +1,7 @@
 function [ modes_m, complex_natural_frequencies_v ] = ...
     F_compute_modes_m( dissipation_m, mass_m, stiffness_m, ...
     varargin )
-%% Coupled modes computation, possible using two different methods
+%% Coupled modes computation
 % Combine the physical matrices describing the system to compute the
 % vibration modes and frequencies.
 %
@@ -13,31 +13,17 @@ function [ modes_m, complex_natural_frequencies_v ] = ...
 %% Arguments parsing
 p = inputParser;
 
-defaultMode = 'first-order';
-validModes = {'ODE', 'first-order'};
-checkMode = @(string) any(validatestring(string, validModes));
-
 addRequired(p, 'dissipation_m', @isnumeric);
 addRequired(p, 'mass_m', @isnumeric);
 addRequired(p, 'stiffness_m', @isnumeric);
-addParameter(p, 'computation_mode_s', defaultMode, checkMode);
 
 parse(p, dissipation_m, mass_m, stiffness_m, varargin{:});
 
-% if strcmp(p.Results.computation_mode_s, 'ODE')
-% %% Undamped modes computation via physical differential equation
-% syms q(t);
-% Dq = diff(q);
-% D2q = diff(q, 2);
-% system_eqn = ( mass_m*D2q + dissipation_m*Dq + stiffness_m*q == 0);
-% dsolve(system_eqn);
-% 
-% else
 %% Damped modes computation via first-order reformulation of the system
 modes_number = length(mass_m);
 inv_mass_m = inv(mass_m);
 combined_system_m = [ zeros(modes_number), eye(modes_number);
-    -inv_mass_m*dissipation_m, -inv_mass_m*stiffness_m];
+    -inv_mass_m*stiffness_m, -inv_mass_m*dissipation_m];
 [ modes_m, complex_natural_frequencies_m ] = ...
     eig(combined_system_m);
 complex_natural_frequencies_v = diag(complex_natural_frequencies_m);
