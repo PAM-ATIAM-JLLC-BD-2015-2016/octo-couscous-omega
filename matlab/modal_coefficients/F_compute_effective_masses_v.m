49d22cf866d6967a4a6929701352f9e781a0982b
tbazin <theis.bazin@ens-cachan.fr>
2016-01-25 21:45:48 +0100

    Cosmetic update

--- matlab/modal_coefficients/F_compute_effective_masses_v.m
+++ matlab/modal_coefficients/F_compute_effective_masses_v.m
@@ -1,17 +1,15 @@
 function [ effective_masses_v ] = F_compute_effective_masses_v( ...
-    body_complex_natural_frequencies_v, body_admittance_v, Fs_Hz)
+    body_admittance_v, Fs_Hz, ...
+    body_pulsations_Hz_v, body_dampings_v)
 %% Body modes effective masses computation
 % Using formula (4.2) from Arthur Paté's PhD thesis.
 
-% How to go from the complex natural frequencies to daming and pulsation?
-% Just real() and imag() or something more?
+dF_admittance_Hz = (Fs_Hz/2) / length(body_admittance_v);
 
-body_pulsations_v = real(body_complex_natural_frequencies_v);
-body_dampings_v = imag(body_complex_natural_frequencies_v);
-
-effective_masses_v = 1 / (...
-    2*abs(body_admittance_v()));
-
+effective_masses_v = ones(length(body_pulsations_Hz_v),1) ./ (eps +...
+    2*abs(body_admittance_v(...
+        floor(body_pulsations_Hz_v/dF_admittance_Hz))) .* ....
+    body_pulsations_Hz_v .* body_dampings_v);
 
 end
 
