8971d10bb0104e7955137a78de2a36a009e726a7
tbazin <theis.bazin@ens-cachan.fr>
2016-01-26 04:01:06 +0100

    Removed celerity (mistake, not necessary here)

--- matlab/F_select_string_parameters.m
+++ matlab/F_select_string_parameters.m
@@ -23,7 +23,6 @@ end
 linear_masses_v = (1e-3)*[6.24, 3.61, 1.95, 0.9, 0.52, 0.38];
 bending_stiffnesses_v = (1e-6)*[57, 40, 51, 310, 160, 130];
 tensions_v = [71.6, 73.9, 71.2, 58.3, 53.4, 70.3];
-celerities_v = [ 107, 143, 191, 255, 321, 429 ];
 
 eta_F_v = (1e-6)*[2,7,5,14,40,40];
 eta_B_v = (1e-5)*[2,2.5,2,2,2,2.4];
@@ -32,7 +31,6 @@ eta_A_v = (1e-2)*[1.2, 0.9, 1.2, 1.7, 1.2, 1.5];
 string_linear_mass = linear_masses_v(note_n);
 string_bending_stiffness = bending_stiffnesses_v(note_n);
 string_tension = tensions_v(note_n);
-string_celerity = celerities_v(note_n);
 eta_F = eta_F_v(note_n);
 eta_B = eta_B_v(note_n);
 eta_A = eta_A_v(note_n);
