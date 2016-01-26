2a37db2bb04770f8d160bb31f71340f7d4876ff0
tbazin <theis.bazin@ens-cachan.fr>
2016-01-26 16:12:38 +0100

    Updated function name

--- matlab/F_compute_string_natural_frequencies_rad_v.m
+++ matlab/F_compute_string_natural_frequencies_rad_v.m
@@ -1,14 +1,15 @@
-function [ string_frequencies_v ] = F_compute_string_frequencies_rad_v( ...
-    string_params, string_modes_number )
+function [ string_natural_frequencies_rad_v ] = ...
+    F_compute_string_natural_frequencies_rad_v( string_params, ...
+    string_modes_number )
 %% String natural frequencies computation via theoretical model
 
 mode_n_v = 1:string_modes_number;
 c = string_params.celerity;
-L = string_params.string_length;
-B = string_params.string_bending_stiffness;
-T = string_params.string_tension;
+L = string_params.length;
+B = string_params.bending_stiffness;
+T = string_params.tension;
 
 % Equation 30 from Woodhouse (a)
-string_frequencies_v = (mode_n_v * pi * c/(L)) .* ...
+string_natural_frequencies_rad_v = (mode_n_v * pi * c/(L)) .* ...
     ( 1 + (B/(2*T)) .* (mode_n_v*pi/L).^2 );
 end
