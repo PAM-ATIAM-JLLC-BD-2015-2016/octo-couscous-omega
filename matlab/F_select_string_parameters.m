547666cc242304c3582abfdefdbce1366caecd01
tbazin <theis.bazin@ens-cachan.fr>
2016-01-26 03:53:46 +0100

    Added string celerity + cosmetic changes

--- matlab/F_select_string_parameters.m
+++ matlab/F_select_string_parameters.m
@@ -1,59 +1,38 @@
-function [ string_params ] = F_select_string_parameters( str_note_name )
+function [ string_linear_mass, string_bending_stiffness, ...
+    string_tension, string_celerity, ...
+    eta_F, eta_B, eta_A ] = F_select_string_parameters( str_note_name )
 %% String parameters selection wrapper, using values provided by Woodhouse
 
 if strcmp(str_note_name,'E2')
-    nb_note = 1;
+    note_n = 1;
 elseif strcmp(str_note_name,'A2')
-    nb_note = 2;
+    note_n = 2;
 elseif strcmp(str_note_name,'D3')
-    nb_note = 3;
+    note_n = 3;
 elseif strcmp(str_note_name,'G3')
-    nb_note = 4;
+    note_n = 4;
 elseif strcmp(str_note_name,'B3')
-    nb_note = 5;
+    note_n = 5;
 elseif strcmp(str_note_name,'E4')
-    nb_note = 6;
+    note_n = 6;
 end             
    
 % Arrays
-linear_mass_arr = (1e-3)*[6.24, 3.61, 1.95, 0.9, 0.52, 0.38];
-bending_stiffness_arr = (1e-6)*[57, 40, 51, 310, 160, 130];
-tension_arr = [71.6, 73.9, 71.2, 58.3, 53.4, 70.3];
-eta_F_arr = (1e-6)*[2,7,5,14,40,40];
-eta_B_arr = (1e-5)*[2,2.5,2,2,2,2.4];
-eta_A_arr = (1e-2)*[1.2, 0.9, 1.2, 1.7, 1.2, 1.5];
+linear_masses_v = (1e-3)*[6.24, 3.61, 1.95, 0.9, 0.52, 0.38];
+bending_stiffnesses_v = (1e-6)*[57, 40, 51, 310, 160, 130];
+tensions_v = [71.6, 73.9, 71.2, 58.3, 53.4, 70.3];
+celerities_v = [ 107, 143, 191, 255, 321, 429 ];
 
-lin_mass = linear_mass_arr(nb_note);
-bend_stif = bending_stiffness_arr(nb_note);
-tens = tension_arr(nb_note);
-etaF = eta_F_arr(nb_note);
-etaB = eta_B_arr(nb_note);
-etaA = eta_A_arr(nb_note);
+eta_F_v = (1e-6)*[2,7,5,14,40,40];
+eta_B_v = (1e-5)*[2,2.5,2,2,2,2.4];
+eta_A_v = (1e-2)*[1.2, 0.9, 1.2, 1.7, 1.2, 1.5];
 
-%% String physical characteristics
-
-string_length = 0.65;
-string_linear_mass = lin_mass;
-string_bending_stiffness = bend_stif;
-string_tension = tens;
-celerity = sqrt(string_tension/string_linear_mass);  
-
-%% Observation parameters
-initial_height = 0.01;  % 1cm
-x_listening = string_length/8;  % Listening point
-x_excitation = string_length/4;                        
-
-%% Parameters container
-string_params = {};
-
-string_params.string_length = string_length;
-string_params.string_linear_mass = string_linear_mass;
-string_params.string_bending_stiffness = string_bending_stiffness;
-string_params.string_tension = string_tension;
-string_params.celerity = celerity;
-
-string_params.initial_height = initial_height;
-string_params.x_listening = x_listening;
-string_params.x_excitation = x_excitation;
+string_linear_mass = linear_masses_v(note_n);
+string_bending_stiffness = bending_stiffnesses_v(note_n);
+string_tension = tensions_v(note_n);
+string_celerity = celerities_v(note_n);
+eta_F = eta_F_v(note_n);
+eta_B = eta_B_v(note_n);
+eta_A = eta_A_v(note_n);
 
 end
