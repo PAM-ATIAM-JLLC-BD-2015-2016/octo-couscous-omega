e5cc97d6b32989a423f4fff41ad8570d96e17111
tbazin <theis.bazin@ens-cachan.fr>
2016-01-26 16:45:24 +0100

    Fixed cohenrency with renamed parameters

--- matlab/F_compute_full_string_parameters.m
+++ matlab/F_compute_full_string_parameters.m
@@ -1,5 +1,5 @@
-function [ string_params ] = ...
-    F_string_parameters( str_note_name, string_modes_number )
+function [ string_params ] = F_compute_full_string_parameters( ...
+    str_note_name, string_modes_number )
 % This function returns a structure with the parameters of the string
 % according to a given note, as well as the frequencies and the damping
 % coefficients.
@@ -8,14 +8,16 @@ function [ string_params ] = ...
 
 
 %% String physical characteristics
+% String specific values
 [string_linear_mass, string_bending_stiffness, string_tension, ...
-    eta_F, eta_B, eta_A] = F_select_string_parameters(str_note_name);
+    eta_F, eta_B, eta_A] = F_select_basic_string_parameters(str_note_name);
 
+% Common values for all strings
 string_length   = 0.65;
 celerity        = sqrt(string_tension/string_linear_mass);
 initial_height  = 0.01;
-x_listening     = string_length/8;  
-x_excitation    = string_length/4;     
+x_listening     = string_length/8;
+x_excitation    = string_length/4;
 
 %% Parameters container
 string_params = {};
@@ -24,17 +26,18 @@ string_params.length             = string_length;
 string_params.linear_mass        = string_linear_mass;
 string_params.bending_stiffness  = string_bending_stiffness;
 string_params.tension            = string_tension;
-string_params.celerity                  = celerity;
-string_params.etaA                      = eta_A;
-string_params.etaB                      = eta_B;
-string_params.etaF                      = eta_F;
+string_params.celerity           = celerity;
+string_params.etaA               = eta_A;
+string_params.etaB               = eta_B;
+string_params.etaF               = eta_F;
 
 string_params.initial_height    = initial_height;
 string_params.x_listening       = x_listening;
 string_params.x_excitation      = x_excitation;
 
 %% String frequencies as described in Woodhouse (a)
-string_natural_frequencies_rad_v = F_compute_string_frequencies_rad_v( ...
+string_natural_frequencies_rad_v = ...
+    F_compute_string_natural_frequencies_rad_v( ...
         string_params, string_modes_number ); 
 
 string_params.natural_frequencies_rad_v = string_natural_frequencies_rad_v;
