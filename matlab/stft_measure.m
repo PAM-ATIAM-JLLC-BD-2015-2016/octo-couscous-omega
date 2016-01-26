897d62fb98dfdfd2735414a292e06d677d8e6015
MaximeDi <max.dickerson@gmail.com>
2016-01-26 23:08:53 +0100

    moved the function stft_pam in the file stft_measure

--- matlab/stft_measure.m
+++ matlab/stft_measure.m
@@ -1,6 +1,4 @@
-
-clear all, close all, clc;
-
+function stft_measure()
 Fs = 25600;
 Nfft = 2^12;
 I = 1;
@@ -24,8 +22,40 @@ figure
 imagesc(ts,freq,res); axis xy
 xlabel('time')
 ylabel('frequency')
-ylim([0 2000])
+ylim([0 1000])
 title('Short term fourier tran5form of acceleration of the guitar bridge');
 
+function Xs = tfct_pam(x,Fe,Nfft,I)
+% analyse d'un signal ? l'aide de la TFCT
+% transform?e de Fourier ? Court Terme
+%clear all; close all;
+
+x=x(:);
+N = length(x); % longueur du signal
+%Nw = 512;
+Nw = Nfft; % Maxime
+w = hanning(Nw); % d?finition de la fenetre d'analyse
+ws = w; % d?finition de la fen?tre de synth?se
+%R = Nw/4; % incr?ment sur les temps d'analyse,
+          % appel? hop size, t_a=uR
+R = I; % longueur d'une trame
+          
+%M = 512; % ordre de la tfd 
+M = Nfft; % ordre de la tfd
+L = M/2+1;
+
+Nt = fix( (N-Nw)/R ); % calcul du nombre de tfd ? calculer
+y = zeros(N,1); % signal de synth?se
+
+Xs = zeros(M,Nt);
+
+for k=1:Nt;  % boucle sur les trame
+   deb = (k-1)*R +1; % d?but de trame
+   fin = deb + Nw -1; % fin de trame
+   tx = x(deb:fin).*w; % calcul de la trame  
+   X = fft(tx,M); % tfd ? l'instant b
+   
+   Xs(:,k)=X;
+end
 
 
