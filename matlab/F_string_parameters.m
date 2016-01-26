63a3e35db8e3f30fce5ef38fd0681b0a48ee32ba
tbazin <theis.bazin@ens-cachan.fr>
2016-01-26 16:23:27 +0100

    Cosmetic updated (shortened field names in teh returned strcu)

--- matlab/F_string_parameters.m
+++ matlab/F_string_parameters.m
@@ -1,4 +1,4 @@
-function [ string_params, string_loss_factors_v ] = ...
+function [ string_params ] = ...
     F_string_parameters( str_note_name, string_modes_number )
 % This function returns a structure with the parameters of the string
 % according to a given note, as well as the frequencies and the damping
@@ -20,10 +20,10 @@ x_excitation    = string_length/4;
 %% Parameters container
 string_params = {};
 
-string_params.string_length             = string_length;
-string_params.string_linear_mass        = string_linear_mass;
-string_params.string_bending_stiffness  = string_bending_stiffness;
-string_params.string_tension            = string_tension;
+string_params.length             = string_length;
+string_params.linear_mass        = string_linear_mass;
+string_params.bending_stiffness  = string_bending_stiffness;
+string_params.tension            = string_tension;
 string_params.celerity                  = celerity;
 string_params.etaA                      = eta_A;
 string_params.etaB                      = eta_B;
@@ -34,13 +34,15 @@ string_params.x_listening       = x_listening;
 string_params.x_excitation      = x_excitation;
 
 %% String frequencies as described in Woodhouse (a)
-string_params.string_frequencies_v = F_compute_string_frequencies_v( ...
-    string_params, string_modes_number ); 
+string_natural_frequencies_rad_v = F_compute_string_frequencies_rad_v( ...
+        string_params, string_modes_number ); 
+
+string_params.natural_frequencies_rad_v = string_natural_frequencies_rad_v;
 
 %% Damping coefficients as described in Woodhouse (b)
 string_loss_factors_v = F_compute_string_loss_factors_v( ...
     string_params, string_modes_number ); 
     
-string_params.string_loss_factors_v = string_loss_factors_v ;
+string_params.loss_factors_v = string_loss_factors_v ;
 
 end
