3fdbc0363a24618566c0e29c5dbfbe9f2711bf94
tbazin <theis.bazin@ens-cachan.fr>
2016-01-31 18:59:36 +0100

    Added support for dirac excitation

--- matlab/modal_coefficients/F_compute_initial_excitation_v.m
+++ matlab/modal_coefficients/F_compute_initial_excitation_v.m
@@ -1,12 +1,15 @@
 function [ initial_excitation_v ] = F_compute_initial_excitation_v( ...
     string_modes_number, string_params, body_modes_number, ...
-    static_height_body, coupled_modes_m, ...
+    static_height_body, coupled_modes_m, type_s, ...
     plots )
 %% Compute projections of the initial displacement over the system's mode
 % Use numerical integration to compute the modal contributions
 
+if nargin < 7
+    plots = false;
 if nargin < 6
-    plots = 0;
+    type_s = 'triangle';
+end
 end
 
 modes_number = string_modes_number + body_modes_number;
@@ -21,10 +24,20 @@ x_excitation_max = x_excitation + excitation_width/2;
 
 L_right = L - x_excitation_max;
 
+if strcmp(type_s, 'triangle')
+   triangle_factor = 1; 
+elseif strcmp(type_s, 'dirac')
+    triangle_factor = 0;
+else
+    error('Unrecognized excitation type')
+end
+
 F_initial_displacement = @(x) (static_height_body * x / L + (...
-        (x <= x_excitation_min ) * H_0 .* (x / x_excitation_min) + ...
+        triangle_factor * (x <= x_excitation_min ) * ...
+            H_0 .* (x / x_excitation_min) + ...
         (x > x_excitation_min & x <= x_excitation_max) * H_0 + ...
-        (x > x_excitation_max) * H_0 .* (L_right - (x-x_excitation_max))/L_right));
+        triangle_factor * (x > x_excitation_max) * ...
+            H_0 .* (L_right - (x-x_excitation_max))/L_right));
 
 if plots
    x_v = (0:1000) * L / 1000;
@@ -35,10 +48,12 @@ end
 initial_excitation_v = zeros(modes_number,1);
 
 for mode_n = 1:modes_number
-    mode_shape_v = coupled_modes_m(1:string_modes_number,mode_n);
+    F_modal_shape = @(x) (sin(mode_n * x * pi/L));
+
+    mode_shape_v = coupled_modes_m(1:string_modes_number,2*mode_n-1);
     F_modal_shape = @(x) (sum(diag(mode_shape_v) * ...
         sin((1:string_modes_number).'*x * pi/L),1));
-    
+
     initial_excitation_v(mode_n) = integral(...
         @(x) (F_initial_displacement(x) .* conj(F_modal_shape(x))), 0, L);
 end
